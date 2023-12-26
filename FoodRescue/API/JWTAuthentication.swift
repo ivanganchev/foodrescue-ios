250ebc7b767e5d79425b7892a504d34189095597
Ivan Ganchev <iganchev@nemetschek.bg>
2023-12-26 18:09:42 +0200

    jwt store and decode

---
+++ FoodRescue/API/JWTAuthentication.swift
@@ -0,0 +1,29 @@
+//
+//  JWTAuthentication.swift
+//  FoodRescue
+//
+//  Created by Ivan Ganchev on 2023-12-26.
+//
+
+import Foundation
+import JWTDecode
+import KeychainSwift
+
+class JWTAuthentication {
+    let keychain = KeychainSwift()
+    
+    func storeJWToken(_ jwt: String) {
+        keychain.set(jwt, forKey: "token")
+    }
+    
+    func getDecodedUserId() -> String? {
+        let jwt = keychain.get("token")
+        
+        guard let jwt = jwt,
+              let decodedToken = try? decode(jwt: jwt) else {
+            return nil
+        }
+        
+        return decodedToken.body["id"] as? String
+    }
+}
---
+++ FoodRescue/Authentication/AuthenticationViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthenticationViewController.swift
+//  FoodRescue
+//
+//  Created by Ivan Ganchev on 2023-12-26.
+//
+
+import Foundation
+
+class AuthenticationViewController {
+    
+}
